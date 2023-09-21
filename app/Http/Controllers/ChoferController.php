638ecdf8e61a85ce4485eead593b97c206484d57
Alan Juarez <alanjuarez.n64@gmail.com>
2023-09-21 17:35:00 -0300

    Se agregaron funciones al controlador ChoferController

--- app/Http/Controllers/ChoferController.php
+++ app/Http/Controllers/ChoferController.php
@@ -2,9 +2,33 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Chofer;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Validation\Rule;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
+
 
 class ChoferController extends Controller
 {
-    //
+    public function Buscar($id){
+        try{
+            $chofer = Chofer::findOrFail($id);
+            return response()->json($chofer);
+
+        }catch(ModelNotFoundException $exception) {
+            return response()->json(['error' => 'chofer no encontrado'], 404);
+        }
+    }
+
+    public function ObtenerCamion($id){
+        try{
+            $chofer = Chofer::findOrFail($id);
+            $camion = $chofer->camion;
+            return response()->json($camion);
+
+        }catch(ModelNotFoundException $exception) {
+            return response()->json(['error' => 'chofer no encontrado'], 404);
+        }
+    }
 }
